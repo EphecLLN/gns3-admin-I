af1335adca791de06ac918d243fe5e17d3cd4c6c
V. Van den Schrieck <v.vandenschrieck@ephec.be>
2024-10-14 11:52:52 -0700

    add super-server

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import os
 import docker
-images = ["base","client", "server", "dhcp", "dns", "web", "mail","router" ] 
+images = ["base","client", "server", "dhcp", "dns", "web", "mail","router","super-server" ] 
 client = docker.from_env()
 for image_name in images :
     if os.path.isdir("./"+image_name) and os.path.exists(f"./{image_name}/Dockerfile") : 
